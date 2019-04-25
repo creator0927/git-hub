e4a52bd9c85899fe6f9655639b1b913732687197
creator0927 <49264108+creator0927@users.noreply.github.com>
2019-04-25 18:11:12 -0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -7,3 +7,4 @@ from first_app.models import AccessRecord, Topic, Webpage
 admin.site.register(AccessRecord)
 admin.site.register(Topic)
 admin.site.register(Webpage)
+admin.site.register(Webproduct)
